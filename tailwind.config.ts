5f6918c88e9c63a89b5e7234e3f1476e32281e9b
Oxfcks <david@goodmonth.io>
2024-01-02 13:54:27 -0800

    mvp

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,16 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        headline: ['Travelers-rounded', 'sans-serif'],
+        body: ['Founders', 'sans-serif'],
       },
+      backgroundImage: {
+        'farm-background' : "url('/farm-background.jpeg')",        
+      },
+      opacity: {
+        '67': '.3',
+      }
     },
   },
   plugins: [],
