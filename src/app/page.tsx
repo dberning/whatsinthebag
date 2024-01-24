5cf045be6b0e4da43a4aec0f0d785706b0a317a9
Oxfcks <david@goodmonth.io>
2024-01-23 21:02:28 -0800

    1-24-24

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default async function Home() {
     <main className="min-w-full flex min-h-screen flex-col">    
     <div className='flex flex-col gap-6 justify-center items-center  mb-20'>
       <h1 className='text-center font-headline text-4xl md:text-7xl px-16'>WHATS IN THE BAG??</h1>                       
-      <div className='flex flex-col md:flex-row flex-wrap gap-4 justify-center items-center'>
+      <div className='md:h-screen md:overflow-y-scroll flex flex-col md:flex-row md:flex-wrap gap-4 justify-center items-center bg-slate-50 w-5/6 md:w-2/3 py-6'>
         {data.length ? data.map( (record: CSAVideo, idx: number) => {
           return <CSACard key={idx} record={record} isFeatured={false}/>
         })
